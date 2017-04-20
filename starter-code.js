c28ca5a9bdf18dc06a538d036841c12e89e7ecd0
bgveenstra <b.g.veenstra@gmail.com>
2017-04-20 11:40:10 -0700

    bring back jean's starter code

---
+++ starter-code.js
@@ -0,0 +1,33 @@
+function mergeSort(arr) {
+  // if the array is length one or zero, return the array
+
+  // figure out the middle point
+
+  // create an array of the left half
+
+  // create an array of right half
+
+  // call merge on a recursively called left half and right half
+
+}
+
+
+function merge(left, right) {
+  //variable to hold result
+
+  // while both arrays have elements in them, zip them together
+
+    // if the left array first element is less than the right array first element, push to result
+
+
+    // else push the right array first element to result
+
+
+  // if left is the only array with elements, push them all in
+
+  // if right is the only array with elmeents, push them all in
+
+
+  // return final result
+
+}
